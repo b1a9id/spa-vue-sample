a3753ce3343fe7313bf3466b8471455bd67857f1
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-01-03 23:22:18 +0900

    initial commit

---
+++ src/main/java/com/example/spavuespring/Application.java
@@ -0,0 +1,12 @@
+package com.example.spavuespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
