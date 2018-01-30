75654b5254193c915bd4368480cf36246b1424b6
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-01-31 00:51:36 +0900

    Implement to show inputted todos

--- src/main/resources/static/js/Hello.vue
+++ src/main/resources/static/js/Hello.vue
@@ -8,6 +8,16 @@
 		<button class="mdl-button mdl-js-button mdl-button--fab" @click="addTodo">
 			<i class="material-icons">add</i>
 		</button>
+		<ul class="demo-list-control mdl-list">
+			<li v-for="todo in filteredTodos" :key="todo.id" class="mdl-list__item">
+				<span class="mdl-list__item-primary-content">{{ todo.title }}</span>
+				<span class="mdl-list__item-secondary-action">
+					<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-1">
+						<input type="checkbox" id="list-checkbox-1" class="mdl-checkbox__input" />
+					</label>
+				</span>
+			</li>
+		</ul>
 	</section>
 </template>
 
@@ -25,13 +35,20 @@
 		save: function (todos) {
 			localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
 		}
-	}
+	};
+
+	const filters = {
+		all: function (todos) {
+			return todos;
+		}
+	};
 
 	export default {
 		data: function () {
 			return {
 				toDos: todoStorage.fetch(),
-				newTodo: ''
+				newTodo: '',
+				visibility: 'all'
 			}
 		},
 		methods: {
@@ -56,6 +73,11 @@
 				deep: true
 			}
 		},
+		computed: {
+			filteredTodos: function () {
+				return filters[this.visibility](this.toDos);
+			}
+		},
 		name: "todo"
 	}
 </script>
@@ -63,5 +85,12 @@
 <style scoped>
 	.message {
 		color: #42b983;
+	}
+	.demo-list-control {
+		width: 300px;
+	}
+
+	.demo-list-radio {
+		display: inline;
 	}
 </style>
